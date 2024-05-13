cf82e6f6179a2c50f8ceeb2920444009d67870d4
曹一梁 <caoyiliang@me.com>
2024-05-13 14:20:43 +0800

    C1-C5

---
+++ GbTest/Behavior/CopyBehavior.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GbTest.Behavior
+{
+    internal class CopyBehavior : Behavior<TextBlock>
+    {
+        protected override void OnAttached()
+        {
+            AssociatedObject.MouseLeftButtonDown += AssociatedObject_MouseLeftButtonDown;
+            base.OnAttached();
+        }
+
+        private void AssociatedObject_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            try
+            {
+                Clipboard.SetDataObject(((TextBlock)sender).Text);
+                MessageBox.Show("已复制");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
--- GbTest/ViewModel/MainViewModel.cs
+++ GbTest/ViewModel/MainViewModel.cs
@@ -8,6 +8,8 @@ using Config.Model;
 using GbTest.Service;
 using HJ212;
 using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics;
+using System.Windows;
 
 namespace GbTest.ViewModel
 {
@@ -23,6 +25,14 @@ namespace GbTest.ViewModel
         private string _MN = "88888888";
         [ObservableProperty]
         private string _PW = "123456";
+        [ObservableProperty]
+        private IEnumerable<ST> _STs;
+        [ObservableProperty]
+        private ST _ST = ST.大气环境污染源;
+        [ObservableProperty]
+        private IEnumerable<HJ212.Version> _Versions;
+        [ObservableProperty]
+        private HJ212.Version _Version = HJ212.Version.HJT212_2017;
 
         private Connection _connection;
         private IGB? _gb;
@@ -30,6 +40,8 @@ namespace GbTest.ViewModel
         {
             _connection = configManager.Connection;
             Status = _connection.ToString();
+            STs = Enum.GetValues(typeof(ST)).Cast<ST>();
+            Versions = Enum.GetValues(typeof(HJ212.Version)).Cast<HJ212.Version>();
             WeakReferenceMessenger.Default.Register<StatusMessage>(this, (r, m) =>
             {
                 _connection = m.Value;
@@ -62,10 +74,10 @@ namespace GbTest.ViewModel
                             DtrEnable = _connection.DTR,
                             RtsEnable = _connection.RTS
                         };
-                        _gb = new GB("串口连接", serialPort, MN, PW);
+                        _gb = new GB("串口连接", serialPort, MN, PW, true, ST, Version);
                         break;
                     case CommunicationType.TcpClient:
-                        _gb = new GB("TCP连接", new TcpClient(_connection.HostName, _connection.Port), MN, PW);
+                        _gb = new GB("TCP连接", new TcpClient(_connection.HostName, _connection.Port), MN, PW, true, ST, Version);
                         break;
                     default: throw new NotSupportedException();
                 }
@@ -109,6 +121,120 @@ namespace GbTest.ViewModel
             }
             return true;
         }
+
+        #region C1
+        [ObservableProperty]
+        private bool _C1;
+        [ObservableProperty]
+        private int? _OverTime = null;
+        [ObservableProperty]
+        private int? _ReCount = null;
+        partial void OnC1Changed(bool value)
+        {
+            if (value)
+            {
+                _gb!.OnSetOverTimeAndReCount += _gb_OnSetOverTimeAndReCount;
+            }
+            else
+            {
+                _gb!.OnSetOverTimeAndReCount -= _gb_OnSetOverTimeAndReCount;
+            }
+        }
+
+        private async Task _gb_OnSetOverTimeAndReCount((int OverTime, int ReCount, HJ212.Model.RspInfo RspInfo) objects)
+        {
+            OverTime = objects.OverTime;
+            ReCount = objects.ReCount;
+            await Task.CompletedTask;
+        }
+        #endregion
+
+        #region C2
+        [ObservableProperty]
+        private bool _C2;
+        partial void OnC2Changed(bool value)
+        {
+            if (value)
+            {
+                _gb!.OnGetSystemTime += MainViewModel_OnGetSystemTime;
+            }
+            else
+            {
+                _gb!.OnGetSystemTime -= MainViewModel_OnGetSystemTime;
+            }
+        }
+
+        private async Task<DateTime?> MainViewModel_OnGetSystemTime((string? PolId, HJ212.Model.RspInfo RspInfo) objects)
+        {
+            return await Task.FromResult(DateTime.Now);
+        }
+        #endregion
+
+        #region C3
+        [ObservableProperty]
+        private bool _C3;
+        [ObservableProperty]
+        private string _SystemTime;
+        partial void OnC3Changed(bool value)
+        {
+            if (value)
+            {
+                _gb!.OnSetSystemTime += MainViewModel_OnSetSystemTime;
+            }
+            else
+            {
+                _gb!.OnSetSystemTime -= MainViewModel_OnSetSystemTime;
+            }
+        }
+
+        private async Task MainViewModel_OnSetSystemTime((string? PolId, DateTime SystemTime, HJ212.Model.RspInfo RspInfo) objects)
+        {
+            SystemTime = objects.SystemTime.ToString("yyyy-MM-dd HH:mm:ss");
+            await Task.CompletedTask;
+        }
+        #endregion
+
+        #region C4
+        [ObservableProperty]
+        private string _PolId_C4 = "w01018";
+        [ObservableProperty]
+        private int _TimeOut_C4 = 120000;
+        [RelayCommand]
+        private async Task C4TestAsync()
+        {
+            try
+            {
+                await _gb!.AskSetSystemTime("a1001", TimeOut_C4);
+            }
+            catch (TimeoutException)
+            {
+                MessageBox.Show("请求超时");
+            }
+        }
+        #endregion
+
+        #region C5
+        [ObservableProperty]
+        private bool _C5;
+        [ObservableProperty]
+        private int _RtdInterval = 30;
+        partial void OnC5Changed(bool value)
+        {
+            if (value)
+            {
+                _gb!.OnGetRealTimeDataInterval += MainViewModel_OnGetRealTimeDataInterval;
+            }
+            else
+            {
+                _gb!.OnGetRealTimeDataInterval -= MainViewModel_OnGetRealTimeDataInterval;
+            }
+        }
+
+        private async Task<int> MainViewModel_OnGetRealTimeDataInterval(HJ212.Model.RspInfo objects)
+        {
+            return await Task.FromResult(RtdInterval);
+        }
+        #endregion
     }
 
     internal class StatusMessage(Connection value) : ValueChangedMessage<Connection>(value) { }
